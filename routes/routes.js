07a19cdc7b280ab8c827037a5faefe4483a7f56e
CAIO GARBIN SILVA <caio.garbin>
2023-09-22 10:13:04 -0300

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -4,8 +4,16 @@ const router = express.Router();
 
 //referência a controllers que serão utilzados nas portas
 const agendasDenunciasController = require('../controllers/agendaDenuncias');
+const anexosController = require('../controllers/anexos');
+const denunciasController = require('../controllers/denuncias');
+const equipesController = require('../controllers/equipes');
+const usuariosController = require('../controllers/usuarios');
 
 //definição de rotas
+router.get('/agendaDenuncias', agendasDenunciasController.listarAgendaDenuncias);
+router.get('/anexos', anexosController.listarAnexos);
+router.get('/denuncias', denunciasController.listaranexos);
+router.get('/agendaDenuncias', agendasDenunciasController.listarAgendaDenuncias);
 router.get('/agendaDenuncias', agendasDenunciasController.listarAgendaDenuncias);
 //post
 //patch
